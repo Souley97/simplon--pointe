1092c416bc5ff48253a4182af71f1c41e1f1e31f
souleymane97 <souleymane9700@gmail.com>
2024-09-18 19:31:35 +0000

    v1

--- app/Http/Controllers/JustificatifController.php
+++ app/Http/Controllers/JustificatifController.php
@@ -11,11 +11,7 @@ class JustificatifController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
-    {
-        //
-    }
-
+   
     /**
      * Show the form for creating a new resource.
      */
